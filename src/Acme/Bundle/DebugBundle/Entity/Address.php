2bb83456e43c6e5608ae86fe0b6741d64cbd57f6
Grégoire Pineau <lyrixx@lyrixx.info>
2014-08-25 16:49:02 +0200

    Entity

---
+++ src/Acme/Bundle/DebugBundle/Entity/Address.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Acme\Bundle\DebugBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Address
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Address
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
---
+++ src/Acme/Bundle/DebugBundle/Entity/Dude.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Acme\Bundle\DebugBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Dude
+ *
+ * @ORM\Table()
+ * @ORM\Entity
+ */
+class Dude
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+}
