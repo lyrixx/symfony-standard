20c6ace31dbc4b67711532d5fc80f65dd0c67541
Grégoire Pineau <lyrixx@lyrixx.info>
2017-09-11 15:40:11 +0200

    Added a reproducer for country

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -4,6 +4,7 @@ namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\CountryType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +14,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        \Locale::setDefault('fr-FR');
+
+        $form = $this->createFormBuilder()
+            ->add('country', CountryType::class)
+            ->getForm()
+        ;
+
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+            'form' => $form->createView(),
         ]);
     }
 }
